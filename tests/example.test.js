e8fd5918bc251143e43a1184ecfd771d99a56927
skshriharik <shrihariskulkarni8@gmail.com>
2020-05-09 01:13:16 +0530

    adding example

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,1 +1,10 @@
-hey history asd
+const puppeteer = require('puppeteer') ; 
+describe('My first Tests' , () => {
+ it ('Should open and close the browser ' , async function() {
+    const browser = await puppeteer.launch({headless:false})
+    const page = await browser.newPage()
+    await page.goto('https://example.com')
+    await page.waitForSelector('h1')
+    await browser.close()
+ })
+})
